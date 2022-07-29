8bd179e87b8bd1ddad6e3376050c4a807bd9ffb7
Kjetil Torgrim Homme <kjetil.homme@redpill-linpro.com>
2022-07-29 17:43:20 +0200

    add AlmaLinux and Rocky as aliases for EL

--- manifests/params/server.pp
+++ manifests/params/server.pp
@@ -1,6 +1,6 @@
 class bareos::params::server {
   case $::operatingsystem {
-    'Ubuntu', 'CentOS', 'RedHat': {
+    'Ubuntu', 'CentOS', 'RedHat', 'AlmaLinux', 'Rocky': {
       $packages = [
         'bareos-database-postgresql',
         'bareos-storage-tape',
